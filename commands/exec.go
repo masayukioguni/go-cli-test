6e70eb6b33a9024ef9ad1d9cf74805e0ac1c9ea1
masayuki_oguni <masayuki_oguni.dev@gmail.com>
2014-11-28 10:47:26 +0900

    add sub cmd test

--- commands/exec.go
+++ commands/exec.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"flag"
 	"fmt"
 )
 
@@ -13,7 +14,29 @@ func (c *ExecCommand) Help() string {
 }
 
 func (c *ExecCommand) Run(args []string) int {
-	fmt.Printf("Run %s\n", c.Cmd)
+	var (
+		boolFlag   bool
+		intFlag    int
+		stringFlag string
+	)
+	cmdFlags := flag.NewFlagSet("build", flag.ContinueOnError)
+	cmdFlags.Usage = func() { c.Help() }
+	cmdFlags.BoolVar(&boolFlag, "bool", true, "bool flag")
+	noArgsBool := cmdFlags.Bool("b", false, "bool flag")
+	cmdFlags.IntVar(&intFlag, "int", 0, "int flag")
+	cmdFlags.IntVar(&intFlag, "i", 0, "int flag")
+	cmdFlags.StringVar(&stringFlag, "string", "blank", "string flag")
+	cmdFlags.StringVar(&stringFlag, "s", "blank", "string flag")
+	if err := cmdFlags.Parse(args); err != nil {
+		return 1
+	}
+	fmt.Println("no args bool flag   =", *noArgsBool)
+
+	fmt.Println("bool flag   =", boolFlag)
+	fmt.Println("int flag    =", intFlag)
+	fmt.Println("string flag =", stringFlag)
+
+	fmt.Printf("Run %s %+v\n", c.Cmd, args)
 	return 1
 }
 
